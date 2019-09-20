576c0befad083ce96bd95c7796e96b37fe12a2a4
James Holton <james_holton@yahoo.co.uk>
2019-09-20 11:03:45 +0100

    Added colored boxes to the screen

--- src/interface.js
+++ src/interface.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
   });
 
   function updateScreen(){
+      updateBoxes();
       $("#temperature").html(thermostat.temperature + "&#176");
       $("#settings").html(thermostat.powerSaveDisplay());
       $("#energyLevel").html(thermostat.currentUsage());
@@ -46,6 +47,35 @@ $(document).ready(function(){
       };
       $("#weather").html(weather);
   };
+
+  function updateBoxes(){
+    temp = thermostat.temperature;
+    blankAllBoxes();
+    if (temp >= 10){$("#temp10").css("background-color", "red");}
+    if (temp >= 12){$("#temp9").css("background-color", "red");}
+    if (temp >= 14){$("#temp8").css("background-color", "red");}
+    if (temp >= 16){$("#temp7").css("background-color", "red");}
+    if (temp >= 18){$("#temp6").css("background-color", "red");}
+    if (temp >= 20){$("#temp5").css("background-color", "red");}
+    if (temp >= 22){$("#temp4").css("background-color", "red");}
+    if (temp >= 24){$("#temp3").css("background-color", "red");}
+    if (temp >= 26){$("#temp2").css("background-color", "red");}
+    if (temp >= 28){$("#temp1").css("background-color", "red");}
+
+  }
+
+  function blankAllBoxes(){
+    $("#temp1").css("background-color", "green");
+    $("#temp2").css("background-color", "green");
+    $("#temp3").css("background-color", "green");
+    $("#temp4").css("background-color", "green");
+    $("#temp5").css("background-color", "green");
+    $("#temp6").css("background-color", "green");
+    $("#temp7").css("background-color", "green");
+    $("#temp8").css("background-color", "green");
+    $("#temp9").css("background-color", "green");
+    $("#temp10").css("background-color", "green");
+  }
 
   function getCityWeather(city){
     $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=69312a7de5429a8d9bdbc71b2e7d1710", function(getWeather) {
